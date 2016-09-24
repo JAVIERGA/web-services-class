a1685c94e81145c7a1a5cdb5e8cda88b7841c731
Jennifer Strater <jlstrater@users.noreply.github.com>
2016-09-24 12:25:54 +0200

    homework 4 - more updates

--- homework4/WhitePages/src/java/com/strater/jenn/WhitePagesService.java
+++ homework4/WhitePages/src/java/com/strater/jenn/WhitePagesService.java
@@ -1,25 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.strater.jenn;
 
 import javax.jws.WebService;
+import java.util.ArrayList;
+import dtu.imm.ws.Person;
+import dtu.imm.ws.Wpfault;
 
-/**
- *
- * @author jstrater
- */
 @WebService(serviceName = "WhitePagesService", portName = "WhitePagesPortTypeBindingPort", endpointInterface = "dtu.imm.ws.WhitePagesPortType", targetNamespace = "ws.imm.dtu", wsdlLocation = "WEB-INF/wsdl/WhitePagesService/WhitePages.wsdl")
 public class WhitePagesService {
+    
+    ArrayList<Person> people = new ArrayList();
 
-    public java.lang.String addPerson(dtu.imm.ws.Person person) throws dtu.imm.ws.Wpfault {
+    public java.lang.String addPerson(Person person) throws Wpfault {
+        people.add(person);
         return "done";
     }
 
-    public dtu.imm.ws.PersonArray findPerson(dtu.imm.ws.Person person) {
+    public dtu.imm.ws.PersonArray findPerson(Person person) {
         return new dtu.imm.ws.PersonArray();
     }
-    
 }
